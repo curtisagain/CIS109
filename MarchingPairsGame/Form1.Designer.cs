cd5686577eaf3f5619e1300fe89608c0494c00a2
Curtis Aloia <curtis.aloia@gmail.com>
2019-09-18 19:52:17 -0700

    Project 1 add a table label panel control to the form

--- MarchingPairsGame/Form1.Designer.cs
+++ MarchingPairsGame/Form1.Designer.cs
@@ -28,21 +28,47 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.TableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
 			this.SuspendLayout();
+			// 
+			// TableLayoutPanel
+			// 
+			this.TableLayoutPanel.AutoSize = true;
+			this.TableLayoutPanel.BackColor = System.Drawing.Color.LemonChiffon;
+			this.TableLayoutPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+			this.TableLayoutPanel.ColumnCount = 4;
+			this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.TableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.TableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+			this.TableLayoutPanel.Name = "TableLayoutPanel";
+			this.TableLayoutPanel.RowCount = 4;
+			this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.TableLayoutPanel.Size = new System.Drawing.Size(532, 503);
+			this.TableLayoutPanel.TabIndex = 0;
 			// 
 			// FormMatchingPairsGame
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(532, 503);
+			this.Controls.Add(this.TableLayoutPanel);
 			this.Name = "FormMatchingPairsGame";
 			this.Text = "Matching Pairs Game";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel TableLayoutPanel;
 	}
 }
 
