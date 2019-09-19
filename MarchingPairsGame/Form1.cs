8a751b84d9e3afdbe75a68dcb6f7c277c69f6156
Curtis Aloia <curtis.aloia@gmail.com>
2019-09-18 20:58:17 -0700

    Project 1 Hide the Icon Images

--- MarchingPairsGame/Form1.cs
+++ MarchingPairsGame/Form1.cs
@@ -40,7 +40,7 @@ namespace MarchingPairsGame
 				{
 					int randomNumber = random.Next(icons.Count);
 					iconLabel.Text = icons[randomNumber];
-
+					iconLabel.ForeColor = iconLabel.BackColor;
 					icons.RemoveAt(randomNumber);
 				}
 			}
