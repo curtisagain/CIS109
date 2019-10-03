06fde41b32dee388d3a2d8c1af5af4159d1f029d
Curtis Aloia <curtis.aloia@gmail.com>
2019-10-02 18:55:01 -0700

    Project 2 adding windows media player

--- MyMusicPlayer/Form1.Designer.cs
+++ MyMusicPlayer/Form1.Designer.cs
@@ -28,7 +28,28 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyMusicPlayer));
+			this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// Player
+			// 
+			this.Player.Enabled = true;
+			this.Player.Location = new System.Drawing.Point(0, 0);
+			this.Player.Name = "Player";
+			this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+			this.Player.Size = new System.Drawing.Size(1061, 296);
+			this.Player.TabIndex = 0;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(0, 264);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(109, 32);
+			this.textBox1.TabIndex = 1;
 			// 
 			// MyMusicPlayer
 			// 
@@ -36,16 +57,23 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.Teal;
 			this.ClientSize = new System.Drawing.Size(1062, 373);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.Player);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.MaximizeBox = false;
 			this.Name = "MyMusicPlayer";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private AxWMPLib.AxWindowsMediaPlayer Player;
+		private System.Windows.Forms.TextBox textBox1;
 	}
 }
 
