72c189a4765d03d1faee5438d650e900e4729bfc
Curtis Aloia <curtis.aloia@gmail.com>
2019-10-11 12:25:20 -0700

    Project 5 Adding Controlls

--- PictureViewer/Form1.Designer.cs
+++ PictureViewer/Form1.Designer.cs
@@ -28,21 +28,146 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.StretchBox = new System.Windows.Forms.CheckBox();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.CloseButton = new System.Windows.Forms.Button();
+			this.BackgroundButton = new System.Windows.Forms.Button();
+			this.ClearButton = new System.Windows.Forms.Button();
+			this.ShowButton = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.flowLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+			this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.StretchBox, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(794, 399);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// StretchBox
+			// 
+			this.StretchBox.AutoSize = true;
+			this.StretchBox.Location = new System.Drawing.Point(3, 408);
+			this.StretchBox.Name = "StretchBox";
+			this.StretchBox.Size = new System.Drawing.Size(60, 17);
+			this.StretchBox.TabIndex = 1;
+			this.StretchBox.Text = "Stretch";
+			this.StretchBox.UseVisualStyleBackColor = true;
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.CloseButton);
+			this.flowLayoutPanel1.Controls.Add(this.BackgroundButton);
+			this.flowLayoutPanel1.Controls.Add(this.ClearButton);
+			this.flowLayoutPanel1.Controls.Add(this.ShowButton);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(123, 408);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(674, 39);
+			this.flowLayoutPanel1.TabIndex = 2;
+			// 
+			// CloseButton
+			// 
+			this.CloseButton.AutoSize = true;
+			this.CloseButton.Location = new System.Drawing.Point(3, 3);
+			this.CloseButton.Name = "CloseButton";
+			this.CloseButton.Size = new System.Drawing.Size(75, 23);
+			this.CloseButton.TabIndex = 0;
+			this.CloseButton.Text = "Close";
+			this.CloseButton.UseVisualStyleBackColor = true;
+			// 
+			// BackgroundButton
+			// 
+			this.BackgroundButton.AutoSize = true;
+			this.BackgroundButton.Location = new System.Drawing.Point(84, 3);
+			this.BackgroundButton.Name = "BackgroundButton";
+			this.BackgroundButton.Size = new System.Drawing.Size(121, 23);
+			this.BackgroundButton.TabIndex = 1;
+			this.BackgroundButton.Text = "Set Background Color";
+			this.BackgroundButton.UseVisualStyleBackColor = true;
+			// 
+			// ClearButton
+			// 
+			this.ClearButton.AutoSize = true;
+			this.ClearButton.Location = new System.Drawing.Point(211, 3);
+			this.ClearButton.Name = "ClearButton";
+			this.ClearButton.Size = new System.Drawing.Size(77, 23);
+			this.ClearButton.TabIndex = 2;
+			this.ClearButton.Text = "Clear Picture";
+			this.ClearButton.UseVisualStyleBackColor = true;
+			// 
+			// ShowButton
+			// 
+			this.ShowButton.AutoSize = true;
+			this.ShowButton.Location = new System.Drawing.Point(294, 3);
+			this.ShowButton.Name = "ShowButton";
+			this.ShowButton.Size = new System.Drawing.Size(80, 23);
+			this.ShowButton.TabIndex = 3;
+			this.ShowButton.Text = "Show Picture";
+			this.ShowButton.UseVisualStyleBackColor = true;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Filter = "JPEG Files (*.jpg) | *.jpg | PNG Files (*.png) | *.png | All files (*.*) | *.*";
+			this.openFileDialog1.Title = "Select a picture file";
 			// 
 			// FormPictureViewer
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.tableLayoutPanel1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "FormPictureViewer";
 			this.Text = "Form1";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.flowLayoutPanel1.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.CheckBox StretchBox;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.Button CloseButton;
+		private System.Windows.Forms.Button BackgroundButton;
+		private System.Windows.Forms.Button ClearButton;
+		private System.Windows.Forms.Button ShowButton;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.ColorDialog colorDialog1;
 	}
 }
 
